264eb0f159fb382d4f8f9211e29ee7f8ca3aa58f
benleong94 <bendemeerben@gmail.com>
2023-11-28 08:54:27 +0800

    Edited

--- src/components/RandomQuote/Quote.jsx
+++ src/components/RandomQuote/Quote.jsx
@@ -3,20 +3,37 @@ import { useState } from 'react'
 import './Quote.css'
 
 function Quote() {
-  const [ quote, setQuote ] = useState({
-    text: "XXXX",
-    author: "YYYY"
-  });
+    const [quote, setQuote] = useState({
+      text: "XXXX",
+      author: "YYYY",
+    });
+
+  let quotes = []
+
+  async function loadQuotes() {
+    const response = await fetch("https://type.fit/api/quotes");
+    quotes = await response.json();
+  }
+
+  const random = () => {
+    const select = quotes[Math.floor(Math.random() * quotes.length)];
+    setQuote(select);
+  }
   
+  loadQuotes()
+
   return (
+    <>    
     <div className="quote-container">
       <div className="quote-text">{quote.text}</div>
       <div className="quote-line"></div>
       <div className="quote-bottom">
-        <div className="author">{quote.author}</div>
-        <button>Next</button>
+        <div className="author">{quote.author.split(',')[0]}</div>
+        <button onClick={() => {random()}}>Next</button>
       </div>
     </div>
+    </>
+
   );
 }
 
