21bc938c0a9bc0649558b8fd6b8690c435d12340
jasperteo <jaspertzj@outlook.sg>
2023-11-30 18:46:28 +0800

    pass inputs to diffrent cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import React, { useState } from "react";
+import React, { useState } from "react"; // eslint-disable-line
 import "./App.css";
-import Data from "./utils";
+import MarketData from "./marketData";
 import logo from "/icons8-stock.svg";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
 function App() {
-  const [symbol, setSymbol] = useState("AAPL");
+  const [symbol, setSymbol] = useState(["AAPL", "MSFT", "GOOG"]);
   const [query, setQuery] = useState("");
   return (
     <>
       <div>
         <img className="logo" src={logo} alt="apple-stocks" />
       </div>
-      <h1>Jasper test project $420.69$</h1>
+      <h1 className="title">PriceQuery Project</h1>
       <div className="input">
         <Stack direction="row" spacing={3}>
           <TextField
@@ -30,21 +31,45 @@ function App() {
             onKeyDown={(e) => e.key === "Enter" && setSymbol(query)}
             onChange={(e) => setQuery(e.target.value.toUpperCase())}
           />
-          <Button variant="contained" onClick={() => setSymbol(query)}>
-            Query<iconify-icon icon="line-md:upload-loop"></iconify-icon>
-          </Button>
+          <ButtonGroup
+            variant="contained"
+            aria-label="outlined primary button group"
+            color="grey"
+          >
+            <Button
+              onClick={() => setSymbol((prev) => prev.toSpliced(0, 1, query))}
+            >
+              Query<iconify-icon icon="line-md:upload-loop"></iconify-icon>
+            </Button>
+            <Button
+              onClick={() => setSymbol((prev) => prev.toSpliced(1, 1, query))}
+            >
+              Query<iconify-icon icon="line-md:upload-loop"></iconify-icon>
+            </Button>
+            <Button
+              onClick={() => setSymbol((prev) => prev.toSpliced(2, 1, query))}
+            >
+              Query<iconify-icon icon="line-md:upload-loop"></iconify-icon>
+            </Button>
+          </ButtonGroup>
         </Stack>
       </div>
       <div className="container">
-        <div className="card">
-          <Data className="title" ticker={symbol} />
+        {" "}
+        {symbol.map((t) => (
+          <div className="card" key={t}>
+            <MarketData ticker={t} />
+          </div>
+        ))}
+        {/* <div className="card">
+          <MarketData ticker={symbol[0]} />
         </div>
         <div className="card">
-          <Data className="title" ticker={symbol} />
+          <MarketData ticker={symbol[1]} />
         </div>
         <div className="card">
-          <Data className="title" ticker={symbol} />
-        </div>
+          <MarketData ticker={symbol[2]} />
+        </div> */}
       </div>
     </>
   );
