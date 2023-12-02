dd8fb702f16608c450f8e28420b416fdd6153ed8
jasperteo <jaspertzj@outlook.sg>
2023-12-03 00:52:31 +0800

    added functionality to add or remove card

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { Stack, Container } from "@mui/system";
-import ButtonGroup from "@mui/material/ButtonGroup";
+import { Button, ButtonGroup } from "@mui/material";
 import Typography from "@mui/joy/Typography";
-import { Input, QueryButton } from "./input-box";
+import Input from "./input-box";
 import OutputCard from "./output-card";
 import doodle from "/logo.svg";
 import polygon from "/polygonio.svg";
@@ -18,35 +18,37 @@ export default function App() {
     <>
       <img className="logo" src={doodle} alt="doodle-chart" />
       <h1 className="title">📈 PriceQuery 📉</h1>
-      <div>
-        <Stack
-          direction="row"
-          spacing={{ xs: 1, sm: 2, md: 4 }}
-          justifyContent="center"
-          useFlexGap
-          flexWrap="wrap">
-          <Input error={error} query={query} setQuery={setQuery} />
-          <ButtonGroup
-            variant="contained"
-            aria-label="outlined primary button group"
-            color="grey">
-            {[...Array(3)].map((ignored, index) => (
-              <QueryButton
-                key={index}
-                index={index}
-                setTicker={setTicker}
-                query={query}
-              />
-            ))}
-          </ButtonGroup>
-        </Stack>
+
+      <Stack
+        direction="row"
+        spacing={{ xs: 1, sm: 2, md: 4 }}
+        justifyContent="center"
+        useFlexGap
+        flexWrap="wrap">
+        <Input error={error} query={query} setQuery={setQuery} />
+        <ButtonGroup
+          variant="contained"
+          aria-label="outlined button group"
+          color="inherit">
+          {[...Array(ticker.length)].map((ignored, index) => (
+            <QueryButton
+              key={index}
+              index={index}
+              setTicker={setTicker}
+              query={query}
+            />
+          ))}
+        </ButtonGroup>
+      </Stack>
+
+      <div className="plus-minus">
+        <PlusMinusButton ticker={ticker} setTicker={setTicker} />
       </div>
       <Container
         sx={{
-          my: "4em",
+          my: "2em",
           display: "flex",
           height: "26em",
-          maxWidth: "48em",
         }}>
         {ticker.map((ticker, index) => (
           <OutputCard
@@ -58,49 +60,94 @@ export default function App() {
           />
         ))}
       </Container>
-      <Stack
-        direction="row"
-        spacing={{ xs: 1.5, sm: 3, md: 4 }}
-        justifyContent="center"
-        useFlexGap
-        flexWrap="wrap">
-        <Typography level="title-md">Made With: </Typography>
-        <a href="https://vitejs.dev/" target="_blank" rel="noreferrer">
-          <iconify-icon icon="logos:vitejs"></iconify-icon>
-        </a>
-        <a href="https://react.dev/" target="_blank" rel="noreferrer">
-          <iconify-icon icon="logos:react"></iconify-icon>
-        </a>
-        <a href="https://bun.sh/" target="_blank" rel="noreferrer">
-          <iconify-icon icon="logos:bun"></iconify-icon>
-        </a>
-        <a href="https://axios-http.com/" target="_blank" rel="noreferrer">
-          <iconify-icon
-            icon="devicon-plain:axios"
-            style={{ color: "#5a29e4" }}></iconify-icon>
-        </a>
-        {/* <a
+      <br />
+      <div className="footer">
+        <Stack
+          direction="row"
+          spacing={{ xs: 1.5, sm: 3, md: 4 }}
+          justifyContent="center"
+          alignItems="center"
+          al>
+          <Typography level="title-lg">Made With: </Typography>
+          <a href="https://vitejs.dev/" target="_blank" rel="noreferrer">
+            <iconify-icon icon="logos:vitejs"></iconify-icon>
+          </a>
+          <a href="https://react.dev/" target="_blank" rel="noreferrer">
+            <iconify-icon icon="logos:react"></iconify-icon>
+          </a>
+          <a href="https://bun.sh/" target="_blank" rel="noreferrer">
+            <iconify-icon icon="logos:bun"></iconify-icon>
+          </a>
+          <a href="https://axios-http.com/" target="_blank" rel="noreferrer">
+            <iconify-icon
+              icon="devicon-plain:axios"
+              style={{ color: "#5a29e4" }}></iconify-icon>
+          </a>
+          {/* <a
           href="https://tanstack.com/query/latest"
           target="_blank"
           rel="noreferrer">
           <iconify-icon icon="logos:react-query-icon"></iconify-icon>
         </a> */}
-        <a href="https://mui.com/" target="_blank" rel="noreferrer">
-          <iconify-icon icon="logos:material-ui"></iconify-icon>
-        </a>
-        <a href="https://polygon.io/" target="_blank" rel="noreferrer">
-          <img src={polygon} alt="polygonio-logo" />
-        </a>
-      </Stack>
-      <br />
-      <Typography level="title-lg">
-        By:{" "}
-        <a href="https://github.com/jasperteo" target="_blank" rel="noreferrer">
-          <iconify-icon icon="line-md:github-loop"></iconify-icon>
-          jasperteo
-        </a>
-      </Typography>
-      <br />
+          <a href="https://mui.com/" target="_blank" rel="noreferrer">
+            <iconify-icon icon="logos:material-ui"></iconify-icon>
+          </a>
+          <a href="https://polygon.io/" target="_blank" rel="noreferrer">
+            <img src={polygon} alt="polygonio-logo" />
+          </a>
+        </Stack>
+        <br />
+        <Typography level="title-lg">
+          By:{" "}
+          <a
+            href="https://github.com/jasperteo"
+            target="_blank"
+            rel="noreferrer">
+            <iconify-icon icon="line-md:github-loop"></iconify-icon>
+            jasperteo
+          </a>
+        </Typography>
+        <br />
+      </div>
     </>
   );
 }
+
+const QueryButton = ({ index, setTicker, query }) => (
+  <Button
+    onClick={() =>
+      query && setTicker((prev) => prev.toSpliced(index, 1, query))
+    }
+    sx={{
+      fontFamily: "Geist Variable",
+      bgcolor: "#444444",
+      color: "#ddd0c8",
+      willChange: "filter",
+      "&:hover": {
+        filter: "drop-shadow(0 0 2em #333333)",
+        transition: "all 0.69s",
+      },
+      "iconify-icon": {
+        height: "1em",
+      },
+    }}>
+    Query<iconify-icon icon="line-md:upload-loop"></iconify-icon>
+  </Button>
+);
+
+const PlusMinusButton = ({ ticker, setTicker }) => (
+  <ButtonGroup variant="text" aria-label="button group" color="inherit">
+    <Button
+      onClick={() =>
+        ticker.length < 6 && setTicker((prev) => [...prev, "AAPL"])
+      }>
+      <iconify-icon icon="line-md:plus-square-twotone"></iconify-icon>
+    </Button>
+    <Button
+      onClick={() =>
+        ticker.length > 2 && setTicker((prev) => prev.toSpliced(-1, 1))
+      }>
+      <iconify-icon icon="line-md:minus-square-twotone"></iconify-icon>
+    </Button>
+  </ButtonGroup>
+);
--- src/input-box.jsx
+++ src/input-box.jsx
@@ -1,44 +1,22 @@
-import { TextField, Button } from "@mui/material";
+import TextField from "@mui/material/TextField";
 
-const Input = (props) => (
-  <TextField
-    error={props.error.some((element) => element !== null)}
-    value={props.query}
-    onChange={(e) => props.setQuery(e.target.value.toUpperCase())}
-    id="tickerInput"
-    label="(e.g. AAPL / C:USDSGD / X:BTCUSD)"
-    variant="filled"
-    placeholder="Type in Stock / FX / Crypto Ticker"
-    color="grey"
-    sx={{
-      width: "21em",
-      "*": {
-        fontFamily: "Geist Variable !important",
-      },
-    }}
-  />
-);
-
-const QueryButton = ({ index, setTicker, query }) => (
-  <Button
-    onClick={() =>
-      query && setTicker((prev) => prev.toSpliced(index, 1, query))
-    }
-    sx={{
-      fontFamily: "Geist Variable",
-      bgcolor: "#444444",
-      color: "#ddd0c8",
-      willChange: "filter",
-      "&:hover": {
-        filter: "drop-shadow(0 0 2em #333333)",
-        transition: "all 0.69s",
-      },
-      "iconify-icon": {
-        height: "1em",
-      },
-    }}>
-    Query<iconify-icon icon="line-md:upload-loop"></iconify-icon>
-  </Button>
-);
-
-export { Input, QueryButton };
+export default function Input(props) {
+  return (
+    <TextField
+      error={props.error.some((element) => element !== null)}
+      value={props.query}
+      onChange={(e) => props.setQuery(e.target.value.toUpperCase())}
+      id="tickerInput"
+      label="(e.g. AAPL / C:USDSGD / X:BTCUSD)"
+      variant="filled"
+      placeholder="Type in Stock / FX / Crypto Ticker"
+      color="grey"
+      sx={{
+        width: "21em",
+        "*": {
+          fontFamily: "Geist Variable !important",
+        },
+      }}
+    />
+  );
+}
