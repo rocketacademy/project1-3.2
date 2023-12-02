680ba44affb01841d34d3b3ed5c6734ddf1a4e01
jasperteo <jaspertzj@outlook.sg>
2023-12-03 01:32:44 +0800

    moved some components around

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { Stack, Container } from "@mui/system";
-import { Button, ButtonGroup } from "@mui/material";
 import Typography from "@mui/joy/Typography";
 import Input from "./input-box";
 import OutputCard from "./output-card";
+import { QueryButton, PlusMinusButton } from "./buttons";
 import doodle from "/logo.svg";
 import polygon from "/polygonio.svg";
 import "./App.css";
@@ -17,7 +17,14 @@ export default function App() {
   return (
     <>
       <img className="logo" src={doodle} alt="doodle-chart" />
-      <h1 className="title">📈 PriceQuery 📉</h1>
+      <Typography
+        level="h1"
+        my="4rem"
+        fontSize="4em"
+        fontWeight="800"
+        color="inherit">
+        📈 PriceQuery 📉
+      </Typography>
 
       <Stack
         direction="row"
@@ -26,24 +33,13 @@ export default function App() {
         useFlexGap
         flexWrap="wrap">
         <Input error={error} query={query} setQuery={setQuery} />
-        <ButtonGroup
-          variant="contained"
-          aria-label="outlined button group"
-          color="inherit">
-          {[...Array(ticker.length)].map((ignored, index) => (
-            <QueryButton
-              key={index}
-              index={index}
-              setTicker={setTicker}
-              query={query}
-            />
-          ))}
-        </ButtonGroup>
+        <QueryButton ticker={ticker} setTicker={setTicker} query={query} />
       </Stack>
 
       <div className="plus-minus">
         <PlusMinusButton ticker={ticker} setTicker={setTicker} />
       </div>
+
       <Container
         sx={{
           my: "2em",
@@ -61,93 +57,57 @@ export default function App() {
         ))}
       </Container>
       <br />
-      <div className="footer">
-        <Stack
-          direction="row"
-          spacing={{ xs: 1.5, sm: 3, md: 4 }}
-          justifyContent="center"
-          alignItems="center"
-          al>
-          <Typography level="title-lg">Made With: </Typography>
-          <a href="https://vitejs.dev/" target="_blank" rel="noreferrer">
-            <iconify-icon icon="logos:vitejs"></iconify-icon>
-          </a>
-          <a href="https://react.dev/" target="_blank" rel="noreferrer">
-            <iconify-icon icon="logos:react"></iconify-icon>
-          </a>
-          <a href="https://bun.sh/" target="_blank" rel="noreferrer">
-            <iconify-icon icon="logos:bun"></iconify-icon>
-          </a>
-          <a href="https://axios-http.com/" target="_blank" rel="noreferrer">
-            <iconify-icon
-              icon="devicon-plain:axios"
-              style={{ color: "#5a29e4" }}></iconify-icon>
-          </a>
-          {/* <a
+
+      <Footer />
+    </>
+  );
+}
+
+const Footer = () => (
+  <div className="footer">
+    <Stack
+      direction="row"
+      spacing={{ xs: 1.5, sm: 3, md: 4 }}
+      justifyContent="center"
+      alignItems="center"
+      al>
+      <Typography level="title-lg">Made With: </Typography>
+      <a href="https://vitejs.dev/" target="_blank" rel="noreferrer">
+        <iconify-icon icon="logos:vitejs"></iconify-icon>
+      </a>
+      <a href="https://react.dev/" target="_blank" rel="noreferrer">
+        <iconify-icon icon="logos:react"></iconify-icon>
+      </a>
+      <a href="https://bun.sh/" target="_blank" rel="noreferrer">
+        <iconify-icon icon="logos:bun"></iconify-icon>
+      </a>
+      <a href="https://axios-http.com/" target="_blank" rel="noreferrer">
+        <iconify-icon
+          icon="devicon-plain:axios"
+          style={{ color: "#5a29e4" }}></iconify-icon>
+      </a>
+      {/* <a
           href="https://tanstack.com/query/latest"
           target="_blank"
           rel="noreferrer">
           <iconify-icon icon="logos:react-query-icon"></iconify-icon>
         </a> */}
-          <a href="https://mui.com/" target="_blank" rel="noreferrer">
-            <iconify-icon icon="logos:material-ui"></iconify-icon>
-          </a>
-          <a href="https://polygon.io/" target="_blank" rel="noreferrer">
-            <img src={polygon} alt="polygonio-logo" />
-          </a>
-        </Stack>
-        <br />
-        <Typography level="title-lg">
-          By:{" "}
-          <a
-            href="https://github.com/jasperteo"
-            target="_blank"
-            rel="noreferrer">
-            <iconify-icon icon="line-md:github-loop"></iconify-icon>
-            jasperteo
-          </a>
-        </Typography>
-        <br />
-      </div>
-    </>
-  );
-}
+      <a href="https://mui.com/" target="_blank" rel="noreferrer">
+        <iconify-icon icon="logos:material-ui"></iconify-icon>
+      </a>
+      <a href="https://polygon.io/" target="_blank" rel="noreferrer">
+        <img src={polygon} alt="polygonio-logo" />
+      </a>
+    </Stack>
+    <br />
 
-const QueryButton = ({ index, setTicker, query }) => (
-  <Button
-    onClick={() =>
-      query && setTicker((prev) => prev.toSpliced(index, 1, query))
-    }
-    sx={{
-      fontFamily: "Geist Variable",
-      bgcolor: "#444444",
-      color: "#ddd0c8",
-      willChange: "filter",
-      "&:hover": {
-        filter: "drop-shadow(0 0 2em #333333)",
-        transition: "all 0.69s",
-      },
-      "iconify-icon": {
-        height: "1em",
-      },
-    }}>
-    Query<iconify-icon icon="line-md:upload-loop"></iconify-icon>
-  </Button>
-);
-
-const PlusMinusButton = ({ ticker, setTicker }) => (
-  <ButtonGroup variant="text" aria-label="button group" color="inherit">
-    <Button
-      onClick={() =>
-        ticker.length < 6 && setTicker((prev) => [...prev, "AAPL"])
-      }>
-      <iconify-icon icon="line-md:plus-square-twotone"></iconify-icon>
-    </Button>
-    <Button
-      onClick={() =>
-        ticker.length > 2 && setTicker((prev) => prev.toSpliced(-1, 1))
-      }>
-      <iconify-icon icon="line-md:minus-square-twotone"></iconify-icon>
-    </Button>
-  </ButtonGroup>
+    <Typography level="title-lg">
+      By:{" "}
+      <a href="https://github.com/jasperteo" target="_blank" rel="noreferrer">
+        <iconify-icon icon="line-md:github-loop"></iconify-icon>
+        jasperteo
+      </a>
+    </Typography>
+    <br />
+  </div>
 );
---
+++ src/buttons.jsx
@@ -0,0 +1,56 @@
+import { Button, ButtonGroup } from "@mui/material";
+
+const QueryButton = ({ ticker, setTicker, query }) => {
+  const SingleButton = (index) => (
+    <Button
+      key={index}
+      index={index}
+      onClick={() =>
+        query && setTicker((prev) => prev.toSpliced(index, 1, query))
+      }
+      sx={{
+        fontFamily: "Geist Variable",
+        bgcolor: "#444444",
+        color: "#ddd0c8",
+        willChange: "filter",
+        "&:hover": {
+          filter: "drop-shadow(0 0 2em #333333)",
+          transition: "all 0.69s",
+        },
+        "iconify-icon": {
+          height: "1em",
+        },
+      }}>
+      Query<iconify-icon icon="line-md:upload-loop"></iconify-icon>
+    </Button>
+  );
+  return (
+    <ButtonGroup
+      variant="contained"
+      aria-label="outlined button group"
+      color="inherit">
+      {[...Array(ticker.length)].map((ignored, index) => SingleButton(index))}
+    </ButtonGroup>
+  );
+};
+
+const PlusMinusButton = ({ ticker, setTicker }) => (
+  <ButtonGroup variant="text" aria-label="button group" color="inherit">
+    {/* Max 6 cards */}
+    <Button
+      onClick={() =>
+        ticker.length < 6 && setTicker((prev) => [...prev, "AAPL"])
+      }>
+      <iconify-icon icon="line-md:plus-square-twotone"></iconify-icon>
+    </Button>
+    {/* Min 2 cards */}
+    <Button
+      onClick={() =>
+        ticker.length > 2 && setTicker((prev) => prev.toSpliced(-1, 1))
+      }>
+      <iconify-icon icon="line-md:minus-square-twotone"></iconify-icon>
+    </Button>
+  </ButtonGroup>
+);
+
+export { QueryButton, PlusMinusButton };
