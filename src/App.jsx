142290da8baba598813f4153b4e20225da15edeb
karenhnlai <karenhnlai@gmail.com>
2023-12-05 11:44:25 +0800

    Upload project

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
-import logo from "/logo.png";
 import "./App.css";
+import { useState } from "react";
+// Import Components
+import Home from "./Components/Home.jsx";
+import Quotes from "./Components/Quotes.jsx";
+import Gratitude from "./Components/Gratitude.jsx";
+import Goals from "./Components/Goals.jsx";
+import Journal from "./Components/Journal.jsx";
+import Navbar from "./Components/Navbar.jsx";
 
-function App() {
+export default function App() {
+  const [currentPage, setCurrentPage] = useState("home");
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  let pageContent;
+  if (currentPage === "home") {
+    pageContent = <Home />;
+  } else if (currentPage === "quotes") {
+    pageContent = <Quotes />;
+  } else if (currentPage === "gratitude") {
+    pageContent = <Gratitude />;
+  } else if (currentPage === "goals") {
+    pageContent = <Goals />;
+  } else if (currentPage === "journal") {
+    pageContent = <Journal />;
+  }
+
   return (
-    <>
-      <div>
-        <img src={logo} className="App-logo" alt="logo" />
-      </div>
-      <h1>Project 1</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    </>
+    <div>
+      <Navbar onPageChange={handlePageChange} currentPage={currentPage} />
+      {pageContent}
+    </div>
   );
 }
-
-export default App;
