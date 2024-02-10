8a73c34605430ca8fb10313ce6eb5ad176441fd2
LAMEfreak <leeweirong@outlook.com>
2024-02-11 01:13:31 +0800

    Deferred Restart feature

--- src/components/Wordle/LostBanner.jsx
+++ src/components/Wordle/LostBanner.jsx
@@ -1,9 +1,5 @@
 import { motion } from "framer-motion";
 
-const handleRestart = () => {
-  console.log(`Restarting the game (lose)`);
-};
-
 const LostBanner = ({ answer }) => {
   return (
     <motion.div
@@ -13,18 +9,21 @@ const LostBanner = ({ answer }) => {
         duration: 0.5,
         ease: [0, 0.71, 0.2, 1.01],
       }}
-      className="fixed inset-0 w-[300px] h-[300px] mx-auto my-auto rounded-md bg-red-800 flex flex-col items-center justify-between px-8 py-8 text-xl"
+      className="fixed inset-0 w-[300px] h-[300px] mx-auto my-auto rounded-md bg-red-800 flex flex-col items-center justify-evenly px-8 py-8 text-xl"
     >
       <div>
         <img src="../../public/lose.svg" alt="lose image" width={100} />
       </div>
-      <div>The answer is {answer}.</div>
-      <button
+      <div>
+        The answer is <strong>{answer}</strong>.
+      </div>
+      <p className="text-base">Refresh the page to play again.</p>
+      {/* <button
         onClick={handleRestart}
         className="w-full rounded-md py-2 bg-red-600 text-lg hover:bg-red-700 transition duration-300 ease-in-out"
       >
         Restart
-      </button>
+      </button> */}
     </motion.div>
   );
 };
--- src/components/Wordle/WonBanner.jsx
+++ src/components/Wordle/WonBanner.jsx
@@ -1,9 +1,5 @@
 import { motion } from "framer-motion";
 
-const handleRestart = () => {
-  console.log(`Restarting the game`);
-};
-
 const WonBanner = ({ numOfGuesses }) => {
   return (
     <motion.div
@@ -13,7 +9,7 @@ const WonBanner = ({ numOfGuesses }) => {
         duration: 0.5,
         ease: [0, 0.71, 0.2, 1.01],
       }}
-      className="fixed inset-0 w-[300px] h-[300px] mx-auto my-auto rounded-md bg-green-800 flex flex-col items-center justify-between px-8 py-10 text-xl"
+      className="fixed inset-0 w-[300px] h-[300px] mx-auto my-auto rounded-md bg-green-800 flex flex-col items-center justify-evenly px-8 py-10 text-xl"
     >
       <div>
         <img src="../../public/win.svg" alt="win image" width={120} />
@@ -24,12 +20,14 @@ const WonBanner = ({ numOfGuesses }) => {
           {numOfGuesses === 1 ? "1 guess" : `${numOfGuesses} guesses`}
         </strong>
       </div>
-      <button
+      <p className="text-base">Refresh the page to play again.</p>
+
+      {/* <button
         onClick={handleRestart}
         className="w-full rounded-md py-2 bg-green-600 text-lg hover:bg-green-700 transition duration-300 ease-in-out"
       >
         Restart
-      </button>
+      </button> */}
     </motion.div>
   );
 };
